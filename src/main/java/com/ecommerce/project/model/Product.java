5ef9977c422eb43a2f3ce51ef79ac895d344c0e2
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-12-21 19:53:15 +0200

    product user relationship

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -29,4 +29,7 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
