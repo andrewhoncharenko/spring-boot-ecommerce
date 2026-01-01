d068cf5c91d22c60e661c8ad030a08ef9f862fd3
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2026-01-01 17:57:24 +0200

    security config

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -1,13 +1,54 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+    @NotBlank
+    @Size(min = 5, message = "Street name must be at least 5 characters")
+    private String streetName;
+    @NotBlank
+    @Size(min = 5, message = "Building name must be at least 5 characters")
+    private String buildingName;
+    @NotBlank
+    @Size(min = 4, message = "City name must be at least 4 characters")
+    private String city;
+    @NotBlank
+    @Size(min = 2, message = "State name must be at least 2 characters")
+    private String state;
+    @NotBlank
+    @Size(min = 2, message = "State name must be at least 2 characters")
+    private String country;
+    @NotBlank
+    @Size(min = 6, message = "PIN code must be at least 6 characters")
+    private String pinCode;
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User>users = new ArrayList<>();
+
+    public Address(String streetName, String buildingName, String city, String state, String country, String pinCode) {
+        this.streetName = streetName;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pinCode = pinCode;
+    }
 }
